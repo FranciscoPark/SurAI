482d64a6a41463edbd56d51813806b23d2490f13
FranciscoPark <76905169+FranciscoPark@users.noreply.github.com>
2024-02-03 00:09:44 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch jyp
    Your branch is up to date with 'origin/jyp'.
    
    Changes to be committed:
    	new file:   court_dection/model.py

---
+++ court_dection/model.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+from keras.src.applications import imagenet_utils
+# For versions <TF2.13 change the above import to:
+# from keras.applications import imagenet_utils
+
+from tensorflow.keras import layers
+from tensorflow import keras
